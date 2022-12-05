365ec412c6a917b7cbc7b8cacf610d03bc4da697
OpsHubTechUser <99649109+OpsHubTech@users.noreply.github.com>
2022-12-05 23:33:43 +0530

    Id::165629 - Deleting test file

--- Test-renamed.java
+++
@@ -1,5 +0,0 @@
-public static void main(String agrs[]){
-  System.out.println("New file");
-  System.out.println("Commit from branch");
-  System.out.println("Commit from branch - modifying lines");
-}
